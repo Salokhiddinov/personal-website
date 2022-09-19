532d2d1127ffd56a062c157a35e184292abe4cff
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-09-20 00:15:45 +0500

    fix: fix slightly

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,6 +19,7 @@ export default function HomePage() {
     setAbout(false);
     setContacts(false);
     setProjects(false);
+    setSkills(false);
   }
   function toggleNavWin() {
     setNavWinOpen(!navWinOpen);
